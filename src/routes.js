e7d4330d84198ddb242d0031291d67a76ac9e6fa
nileshgulia1 <nileshgulia@gmail.com>
2024-06-28 15:22:17 +0530

    update jsconfig.json and remove prefixPath things from routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,7 @@
  * @module routes
  */
 
-import { NotFound } from '@plone/volto/components';
-import { defaultRoutes, multilingualRoutes } from '@plone/volto/routes';
+import { defaultRoutes } from '@plone/volto/routes';
 import config from '@plone/volto/registry';
 
 /**
@@ -12,45 +11,14 @@ import config from '@plone/volto/registry';
  * @array
  * @returns {array} Routes.
  */
-// const routes = [
-//   {
-//     path: '/',
-//     component: App, // Change this if you want a different component
-//     routes: [
-//       // Add your routes here
-//       // addon routes have a higher priority then default routes
-//       ...(config.addonRoutes || []),
-//       ...defaultRoutes,
-//     ],
-//   },
-// ];
 const routes = [
   {
-    path: config.settings.prefixPath || '/',
-    component: config.getComponent('App').component,
+    path: '/',
+    component: config.getComponent('App').component, // Change this if you want a different component
     routes: [
-      // redirect to external links if path is in blacklist
-      ...(config.settings?.externalRoutes || []).map((route) => ({
-        ...route.match,
-        component: NotFound,
-      })),
-      ...[
-        // addon routes have a higher priority then default routes
-        ...(config.addonRoutes || []),
-        ...((config.settings?.isMultilingual && multilingualRoutes) || []),
-        ...defaultRoutes,
-      ].map((route) =>
-        config.settings.prefixPath
-          ? {
-              ...route,
-              path: Array.isArray(route.path)
-                ? route.path.map(
-                    (path) => `${config.settings.prefixPath}${path}`,
-                  )
-                : `${config.settings.prefixPath}${route.path}`,
-            }
-          : route,
-      ),
+      // Add your routes here
+      ...(config.addonRoutes || []),
+      ...defaultRoutes,
     ],
   },
 ];
